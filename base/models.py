db074f096cfe5ce5b0767a627fd808dc95673ac4
sonu <skjais04@gmail.com>
2025-11-12 12:54:17 +0545

    crud done

--- base/models.py
+++ base/models.py
@@ -19,6 +19,11 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated','-created']
+
+    # -updated sort in descending order 
+
     def __str__(self):
         return self.name
 
