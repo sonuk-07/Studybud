88fd8206cefc1a92ff096022bdd86da56bdf9198
sonu <skjais04@gmail.com>
2025-11-12 12:15:52 +0545

    topic message room database created

---
+++ base/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Room
+
+# rooms = [
+#     {'id':1, 'name':'lets learn python'},
+#     {'id':2, 'name':'Design with me'},
+#     {'id':3, 'name':'Frontend developer'},
+# ]
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html',context)
+
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request, 'base/room.html',context)
